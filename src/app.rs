e37e13e9f8b53b9c9e2a969604df19500cf73d27
Adam Calvelage <adamjasoncalvelage@gmail.com>
2024-12-27 02:53:16 -0500

    Added egui file dialog

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,19 @@
 use crate::AssetBrowser;
+use egui_file_dialog::FileDialog;
+use std::path::PathBuf;
 
 pub struct App {
     asset_browser: AssetBrowser,
+    file_dialog: FileDialog,
+    picked_file: Option<PathBuf>,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
             asset_browser: Default::default(),
+            file_dialog: FileDialog::new(),
+            picked_file: None,
         }
     }
 }
@@ -22,10 +28,12 @@ impl App {
             ui.menu_button("File", |ui| {
                 if ui.button("Load File").clicked() {
                     ui.close_menu();
+                    self.file_dialog.pick_file();
                 }
     
                 if ui.button("Export Selected").clicked() {
                     ui.close_menu();
+                    println!("EXPORT");
                 }
     
                 if ui.button("Quit").clicked() {
@@ -34,6 +42,13 @@ impl App {
                 }
             });
             ui.add_space(16.0);
+
+            self.file_dialog.update(ctx);
+
+            if let Some(path) = self.file_dialog.take_picked() {
+                self.picked_file = Some(path.to_path_buf());
+                println!("{}", path.to_path_buf().display());
+            }
     
             egui::widgets::global_theme_preference_buttons(ui);
         });
