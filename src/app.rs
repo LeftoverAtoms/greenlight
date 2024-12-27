6b4fc32b0ad6fcbb4c4152da290b116877a01253
Adam Calvelage <adamjasoncalvelage@gmail.com>
2024-12-27 03:06:09 -0500

    Rework drawing

--- src/app.rs
+++ src/app.rs
@@ -16,27 +16,38 @@ impl App {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         Default::default()
     }
+
+    fn update_header(&mut self, ctx: &egui::Context, ui:&mut egui::Ui) {
+        egui::menu::bar(ui, |ui| {
+            ui.menu_button("File", |ui| {
+                if ui.button("Load File").clicked() {
+                    ui.close_menu();
+                }
+    
+                if ui.button("Export Selected").clicked() {
+                    ui.close_menu();
+                }
+    
+                if ui.button("Quit").clicked() {
+                    ui.close_menu();
+                    ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                }
+            });
+            ui.add_space(16.0);
+    
+            egui::widgets::global_theme_preference_buttons(ui);
+        });
+    }
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            update_header(ctx, ui);
+            self.update_header(ctx, ui);
             ui.add_space(8.0);
-            self.asset_browser.show(ctx, ui);
+            self.asset_browser.show_search(ui);
+            ui.add_space(8.0);
+            self.asset_browser.show_table(ui);
         });
     }
-}
-
-fn update_header(ctx: &egui::Context, ui:&mut egui::Ui) {
-    egui::menu::bar(ui, |ui| {
-        ui.menu_button("File", |ui| {
-            if ui.button("Quit").clicked() {
-                ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-            }
-        });
-        ui.add_space(16.0);
-
-        egui::widgets::global_theme_preference_buttons(ui);
-    });
 }
--- src/asset_browser.rs
+++ src/asset_browser.rs
@@ -7,39 +7,46 @@ pub struct AssetBrowser {
 }
 
 impl AssetBrowser {
-    pub fn show(&mut self, ctx: &egui::Context, ui: &mut Ui) {
-        self.show_search(ui);
-
-        ui.vertical_centered_justified(|ui| {
-            self.show_table(ui);
-        });
-    }
-
-    fn show_search(&mut self, ui: &mut Ui) {
+    pub fn show_search(&mut self, ui: &mut Ui) {
         egui::TextEdit::singleline(&mut self.text)
             .hint_text("Search")
             .show(ui);
     }
 
-    fn show_table(&mut self, ui: &mut Ui) {
+    pub fn show_table(&mut self, ui: &mut Ui) {
         TableBuilder::new(ui)
             // Style.
             .cell_layout(Layout::left_to_right(Align::Center))
             .striped(true)
+            .resizable(true)
             // Must allocate space foreach column.
+            .column(Column::remainder())
+            .column(Column::remainder())
             .column(Column::remainder())
             // ...
             .header(20.0, |mut header| {
                 // Define each column.
                 header.col(|ui| {
-                    ui.strong("Interaction");
+                    ui.strong("Asset Name");
+                });
+                header.col(|ui| {
+                    ui.strong("Type");
+                });
+                header.col(|ui| {
+                    ui.strong("Details");
                 });
             })
             .body(|mut body| {
                 for row_index in 0..8 {
                     body.row(64.0, |mut row| {
                         row.col(|ui| {
-                            ui.checkbox(&mut true, "Click me");
+                            ui.label("c_zom_player_cia_fb");
+                        });
+                        row.col(|ui| {
+                            ui.label("Model");
+                        });
+                        row.col(|ui| {
+                            ui.label("Bones: 102, LODs: 4");
                         });
                     });
                 }
